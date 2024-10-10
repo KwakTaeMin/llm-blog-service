bf6aba9bf5f59222e44a77244ae097d03d929ab8
곽태민 <taemin@lezhin.com>
2024-10-10 17:13:03 +0900

    init project

---
+++ src/main/java/com/taemin/llm/LlmBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.taemin.llm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmBlogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmBlogServiceApplication.class, args);
+    }
+
+}
